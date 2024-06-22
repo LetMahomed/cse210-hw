5fd8b63a0ea62c3d54a7140e98cb26f6d2df8216
LetMahomed <des22002@byui.edu>
2024-06-22 19:41:33 -0300

    resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Purchasing Assistant";
+        job1._company = "EZParts";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "English Teacher";
+        job2._company = "Kingdom";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Letícia Mahomed";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
