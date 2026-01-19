746ad45500c9a2dc4f0bde5f5d2b949c9c619104
LetMahomed <ldesousa@byupathway.edu>
2026-01-19 18:50:01 -0300

    Week2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,68 @@
 using System;
 
-class Program
+namespace DailyJournal
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+
+            List<string> prompts = new List<string>
+            {
+                "What is one thing you learned today, and how can you apply it in the future?",
+                "Who did you help today, and how did it make you feel?",
+                "What challenge did you face today, and how did you overcome it?",
+                "What are you grateful for today?",
+                "What is one thing you could have done differently today?",
+                "How did I take care of my physical and mental health today?",
+            };
+
+            bool quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("\nWelcome to the Journal Program");
+                Console.WriteLine("Please Select one of the following choices:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Save");
+                Console.WriteLine("4. Load");
+                Console.WriteLine("5. Quit");
+
+                Console.WriteLine("\nWhat would you like to do? ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        string prompt = GetRandomPrompt(prompts);
+                        journal.AddEntry(prompt);
+                        break;
+                    case "2":
+                        journal.DisplayEntries();
+                        break;
+                    case "3":
+                        journal.SaveToFile();
+                        break;
+                    case "4":
+                        journal.LoadFromFile();
+                        break;
+                    case "5":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+            Console.WriteLine("Until next time, keep writing!");
+        }
+
+        static string GetRandomPrompt(List<string> prompts)
+        {
+            Random random = new Random();
+            int index = random.Next(prompts.Count);
+            return prompts[index];
+        }
     }
 }
