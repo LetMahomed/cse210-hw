20017288a340a35df6119a69c2b5d884017159e9
LetMahomed <ldesousa@byupathway.edu>
2026-01-27 23:55:22 -0300

    Scripture

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+namespace DailyScripture
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        
+        static void Main()
+        {
+            Scripture scripture = new Scripture("Alma ", 32, 21, "And now as I said concerning faith—faith is not to have a perfect knowledge of things; therefore if ye have faith ye hope for things which are not seen, which are true. ");
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nGood job! You memorized the scripture!!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
 }
