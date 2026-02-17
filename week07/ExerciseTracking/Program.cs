883d93777afbc5f74f8a8a6b3bcad2c5ff76fe9c
LetMahomed <ldesousa@byupathway.edu>
2026-02-17 10:17:03 -0300

    Exercising

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("Feb 1, 2026", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("Feb 12, 2026", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("Feb 20, 2026", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
